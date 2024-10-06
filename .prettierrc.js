a3c85bbb5512eec49d3dec02448806d9fac1c0f3
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2024-10-06 18:43:58 -0400

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  printWidth: 160,
+  endOfLine: 'auto',
+};
